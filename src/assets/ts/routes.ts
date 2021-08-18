f477dfa86201f01295c4c334663fc212bd36e94b
Michell Algarra <michell.algarra@gmail.com>
2021-08-18 19:08:26 -0300

    feat(routes.ts): changed path to index.html

--- src/assets/ts/routes.ts
+++ src/assets/ts/routes.ts
@@ -5,7 +5,7 @@ import path from 'path'
 const router = Router()
 
 router.get('/', (_, res) => {
-  res.sendFile(path.resolve(__dirname, './assets/index.html'))
+  res.sendFile(path.resolve(__dirname, './index.html'))
 })
 
 export default router
