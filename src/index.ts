96c98af7668492a220f4c460b3e7c4319ca600ff
Michell Algarra <michell.algarra@gmail.com>
2021-08-18 17:51:30 -0300

    refactor(index.ts): removed express routes and socket connections and imported it's modules

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,17 @@
 import express from 'express'
-import { io } from 'socket.io-client'
-
 import path from 'path'
 import { config } from 'dotenv'
 
+import routes from './assets/ts/routes'
+import './assets/ts/socket'
+
 config()
 
-const socketApiUrl: string =
-  process.env.SOCKET_API_URL || 'http://localhost:3001'
 const port = process.env.PORT || 8080
 
 const app = express()
 
-const socket = io(socketApiUrl)
-
-app.use(express.static(path.resolve(__dirname + '../dist/assets')))
-
-app.get('/', (_, res) => {
-  res.sendFile(path.resolve(__dirname + '../dist/assets/html/index.html'))
-})
-
-socket.on('connect', () => {
-  console.log('Connected to server: ' + socket.id)
-})
-
-socket.on('disconnect', () => {
-  console.log('Disconnected from server: ' + socket.id)
-})
+app.use(express.static(path.resolve(__dirname, '../dist/assets')))
+app.use('/', routes)
 
 app.listen(port)
