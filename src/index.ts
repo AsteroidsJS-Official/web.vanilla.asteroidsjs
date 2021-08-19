55c82dd268017cec4d00618e4533352a01bfcbc8
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-08-18 22:12:54 -0300

    feat(routes.ts & socket.ts & index.ts): removed some files from the "ts" folder; imported the "global.scss" in the "index.ts" file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import express from 'express'
-
-import routes from './assets/ts/routes'
-import { config } from 'dotenv'
-import path from 'path'
-
-import './assets/ts/socket'
 import './global.scss'
-
-config()
-
-const app = express()
-
-app.use(express.static(path.resolve(__dirname, '../dist/assets')))
-app.use('/', routes)
-
-app.listen(process.env.PORT || 8080)
