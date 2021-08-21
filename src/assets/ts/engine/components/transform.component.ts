78732a5a945345f5b4281b5bbd1b3ae86f6d249e
Jose Bieco <jo_bieco@hotmail.com>
2021-08-20 21:50:05 -0300

    refactor(transform.component.ts): set default position and the dimensions

--- src/assets/ts/engine/components/transform.component.ts
+++ src/assets/ts/engine/components/transform.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '../core/component'
 import { Vector2 } from '../core/vector2'
-import { IStart } from '../interfaces/start.interface'
 
-export class Transform extends Component implements IStart {
-  public position: Vector2
-  public dimensions: Vector2
+export class Transform extends Component {
+  public position: Vector2 = new Vector2()
+  public dimensions: Vector2 = new Vector2(100, 100)
 
   public get canvasPosition(): Vector2 {
     return new Vector2(
       this.game.context.canvas.width / 2 + this.position.x,
       this.game.context.canvas.height / 2 - this.position.y,
     )
-  }
-
-  public start(): void {
-    this.position = new Vector2()
-    this.dimensions = new Vector2(100, 100)
   }
 }
