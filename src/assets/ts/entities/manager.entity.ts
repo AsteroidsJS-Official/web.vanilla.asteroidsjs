5aa5fa248976dd2a6bd23386b41b6d6b29939d87
Jose Bieco <jo_bieco@hotmail.com>
2021-08-20 21:52:24 -0300

    feat(manager.entity.ts): set spaceship

--- src/assets/ts/entities/manager.entity.ts
+++ src/assets/ts/entities/manager.entity.ts
@@ -1,15 +1,17 @@
 import { AvoidOverflow } from '../engine/components/avoid-overflow.component'
+import { Input } from '../engine/components/input.component'
 import { Rigidbody } from '../engine/components/rigidbody.component'
 import { Transform } from '../engine/components/transform.component'
 import { Entity } from '../engine/core/entity'
 import { IStart } from '../engine/interfaces/start.interface'
 import { Ball } from './ball.entity'
+import { Spaceship } from './spaceship.entity'
 
 export class Manager extends Entity implements IStart {
   public start(): void {
     this.instantiate({
-      entity: Ball,
-      components: [Transform, Rigidbody, AvoidOverflow],
+      entity: Spaceship,
+      components: [Transform, Rigidbody, AvoidOverflow, Input],
     })
   }
 }
