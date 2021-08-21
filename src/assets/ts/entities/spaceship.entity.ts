3c754acb17b2507f85f4f8ccefc2031d5e51bcb9
Jose Bieco <jo_bieco@hotmail.com>
2021-08-20 21:51:13 -0300

    feat(spaceship.entity.ts): set max velocity and dimensions.

--- src/assets/ts/entities/spaceship.entity.ts
+++ src/assets/ts/entities/spaceship.entity.ts
@@ -1,13 +1,20 @@
+import { Rigidbody } from '../engine/components/rigidbody.component'
 import { Transform } from '../engine/components/transform.component'
 import { Entity } from '../engine/core/entity'
+import { Vector2 } from '../engine/core/vector2'
 import { IDraw } from '../engine/interfaces/draw.interface'
 import { IStart } from '../engine/interfaces/start.interface'
 
 export class Spaceship extends Entity implements IStart, IDraw {
   private transform: Transform
+  private rigidbody: Rigidbody
 
   public start(): void {
     this.transform = this.getComponent(Transform)
+    this.rigidbody = this.getComponent(Rigidbody)
+
+    this.transform.dimensions = new Vector2(50, 50);
+    this.rigidbody.maxVelocity = 4;
   }
 
   public draw(): void {
