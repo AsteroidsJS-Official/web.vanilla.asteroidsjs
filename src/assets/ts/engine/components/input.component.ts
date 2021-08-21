6d3b3e28f0faa31acf03155df3c36bb8035c9b69
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-08-21 18:30:07 -0300

    feat(entities/* & input.component.ts): implemented the spaceship rotation based on the angular velocity

--- src/assets/ts/engine/components/input.component.ts
+++ src/assets/ts/engine/components/input.component.ts
@@ -1,35 +1,38 @@
-import { IGameKeys } from '../../interfaces/input.interface';
+import { IGameKeys } from '../../interfaces/input.interface'
+import { ISpaceship } from '../../interfaces/spaceship.interface'
 import { Component } from '../core/component'
-import { Vector2 } from '../core/vector2'
 import { IStart } from '../interfaces/start.interface'
-import { Rigidbody } from './rigidbody.component';
-import { fromEvent } from 'rxjs';
+import { Vector2 } from '../math/vector2'
+import { Rigidbody } from './rigidbody.component'
+import { fromEvent } from 'rxjs'
 
-export class Input extends Component implements IStart{
-  private rigidbody: Rigidbody;
-  private readonly force: number = 0.05;
-  private gameKeys: IGameKeys = {};
+export class Input extends Component implements IStart {
+  private gameKeys: IGameKeys = {}
+  private spaceship: ISpaceship
+  private rigidbody: Rigidbody
 
   public start(): void {
-    this.rigidbody = this.getComponent(Rigidbody);
-    this.rigidbody.mass = 5;
-    this.keyPressed();
+    this.requires([Rigidbody])
+
+    this.spaceship = this.entity as unknown as ISpaceship
+    this.rigidbody = this.getComponent(Rigidbody)
+
+    this.keyPressed()
   }
 
   /**
    * Captures the pressed key and checks the corresponding action
    */
-  public keyPressed(): void{
+  public keyPressed(): void {
     fromEvent(window, 'keydown').subscribe((e: KeyboardEvent) => {
-      this.setGameKeyPressed(e.key, true);
-      this.move();
-    });
+      this.setGameKeyPressed(e.key, true)
+      this.move()
+    })
 
     fromEvent(window, 'keyup').subscribe((e: KeyboardEvent) => {
-      this.setGameKeyPressed(e.key, false);
-      this.move();
-    });
-
+      this.setGameKeyPressed(e.key, false)
+      this.move()
+    })
   }
 
   /**
@@ -37,50 +40,51 @@ export class Input extends Component implements IStart{
    * @param key string that represents the key pressed
    */
   private setGameKeyPressed(key: string, isPreseed: boolean): void {
-    switch(key) {
-      case "W":
-      case "ArrowUp":
-        this.gameKeys["up"] =  isPreseed;
-        break;
+    switch (key) {
+      case 'W':
+      case 'ArrowUp':
+        this.gameKeys['up'] = isPreseed
+        break
 
-      case "A":
-      case "ArrowLeft":
-        this.gameKeys["left"] =  isPreseed;
-        break;
+      case 'A':
+      case 'ArrowLeft':
+        this.gameKeys['left'] = isPreseed
+        break
 
-      case "S":
-      case "ArrowDown":
-        this.gameKeys["down"] =  isPreseed;
-        break;
+      case 'S':
+      case 'ArrowDown':
+        this.gameKeys['down'] = isPreseed
+        break
 
-      case "D":
-      case "ArrowRight":
-        this.gameKeys["right"] =  isPreseed;
-        break;
+      case 'D':
+      case 'ArrowRight':
+        this.gameKeys['right'] = isPreseed
+        break
     }
   }
 
-  /**
-   *
-   */
   private move(): void {
-    if(!Object.entries(this.gameKeys).map((item) => item[1]).reduce((prev, cur) =>prev || cur)) {
-      this.rigidbody.resultant = new Vector2();
-      return;
+    if (
+      !Object.entries(this.gameKeys)
+        .map((item) => item[1])
+        .reduce((prev, cur) => prev || cur)
+    ) {
+      this.rigidbody.resultant = new Vector2()
+      this.rigidbody.angularResultant = 0
+      return
     }
-    for(const key in this.gameKeys){
-      if(this.gameKeys[key] && key === "up"){
-        this.rigidbody.resultant = Vector2.sum(this.rigidbody.resultant, new Vector2(0, this.force));
+    for (const key in this.gameKeys) {
+      if (this.gameKeys[key] && key === 'up') {
+        this.rigidbody.resultant = Vector2.sum(
+          this.rigidbody.resultant,
+          Vector2.multiply(this.spaceship.direction, this.spaceship.force),
+        )
       }
-      if(this.gameKeys[key] && key === "down"){
-        this.rigidbody.resultant = Vector2.sum(this.rigidbody.resultant, new Vector2(0, -this.force));
+      if (this.gameKeys[key] && key === 'right') {
+        this.rigidbody.angularResultant += this.spaceship.angularForce
       }
-      if(this.gameKeys[key] && key === "right"){
-        this.rigidbody.resultant = Vector2.sum(this.rigidbody.resultant, new Vector2(this.force, 0));
-        console.log(this.rigidbody.resultant)
-      }
-      if(this.gameKeys[key] && key === "left"){
-        this.rigidbody.resultant = Vector2.sum(this.rigidbody.resultant, new Vector2(-this.force, 0));
+      if (this.gameKeys[key] && key === 'left') {
+        this.rigidbody.angularResultant += -this.spaceship.angularForce
       }
     }
   }
