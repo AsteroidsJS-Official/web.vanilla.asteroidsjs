f6331d0489907910cbcb11063325493b74647f0f
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-08-21 18:27:54 -0300

    refactor(math/*): reorganized the classes related with math into the "math" folder

--- src/assets/ts/engine/components/rigidbody.component.ts
+++ src/assets/ts/engine/components/rigidbody.component.ts
@@ -1,29 +1,43 @@
 import { Component } from '../core/component'
-import { Vector2 } from '../core/vector2'
 import { ILoop } from '../interfaces/loop.interface'
 import { IStart } from '../interfaces/start.interface'
+import { Vector2 } from '../math/vector2'
 import { Transform } from './transform.component'
 
 export class Rigidbody extends Component implements IStart, ILoop {
   public mass = 1
   public resultant = new Vector2()
-  public maxVelocity = Infinity;
+  public angularResultant = 0
+  public maxAngularVelocity = Infinity
+  public maxVelocity = Infinity
 
+  public _angularVelocity = 0
   private _velocity = new Vector2()
+
   private transform: Transform
 
   public get velocity(): Vector2 {
-    return this._velocity;
+    return this._velocity
   }
 
-  public set velocity(velocity: Vector2){
-    if(velocity.magnitude > this.maxVelocity){
-      const result = this.maxVelocity / velocity.magnitude;
-
-      velocity = new Vector2(velocity.x * result, velocity.y * result);
+  public set velocity(vector: Vector2) {
+    if (vector.magnitude > this.maxVelocity) {
+      const result = this.maxVelocity / vector.magnitude
+      vector = new Vector2(vector.x * result, vector.y * result)
     }
+    this._velocity = vector
+  }
 
-    this._velocity = velocity;
+  public get angularVelocity(): number {
+    return this._angularVelocity
+  }
+
+  public set angularVelocity(value: number) {
+    const normalized =
+      this.abs(value) > this.maxAngularVelocity
+        ? this.maxAngularVelocity
+        : this.abs(value)
+    this._angularVelocity = value < 0 ? -normalized : normalized
   }
 
   public start(): void {
@@ -33,12 +47,24 @@ export class Rigidbody extends Component implements IStart, ILoop {
   }
 
   public loop(): void {
-    const aceleration = Vector2.divide(
+    const angularAceleration = this.angularResultant / this.mass
+
+    this.angularVelocity += angularAceleration
+    this.transform.rotation += this.angularVelocity
+
+    const aceleration = Vector2.multiply(
       new Vector2(this.resultant.x, this.resultant.y),
-      this.mass,
+      1 / this.mass,
     )
 
-    this.transform.position = Vector2.sum(this.transform.position, this.velocity)
+    this.transform.position = Vector2.sum(
+      this.transform.position,
+      this.velocity,
+    )
     this.velocity = Vector2.sum(this.velocity, aceleration)
+  }
+
+  private abs(value: number): number {
+    return value < 0 ? -value : value
   }
 }
