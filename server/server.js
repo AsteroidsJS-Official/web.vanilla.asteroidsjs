847a455a197f76f9a8eaaa21c74ce34a2949d8b8
Michell Algarra <michell.algarra@gmail.com>
2021-08-23 19:19:21 -0300

    feat(server/*): tweaked the server to ensure the screen reloading data getting

--- server/server.js
+++ server/server.js
@@ -10,11 +10,17 @@ dotenv.config()
 /**
  * @typedef {Object} Screen
  * @property {string} id - The screen id.
- * @property {boolean} isConnected - Whether the screen is connected or not.
  * @property {number} number - The screen number.
  * @property {number} width - The screen width.
  * @property {number} height - The screen height.
  */
+
+/**
+ * The arguments passed by the command line.
+ *
+ * @type {string[]}
+ */
+const args = process.argv.slice(2)
 
 /**
  * Represents the connected screens.
@@ -28,16 +34,21 @@ let screens = {}
  *
  * @type {number}
  */
-let screenAmount = 3
+let screenAmount =
+  args.find((arg) => arg.includes('nscreens'))?.split('=')[1] ||
+  process.env.SCREEN_AMOUNT ||
+  3
 
 /**
+ * The screen socket for screen connections.
+ *
  * @type {Server<import('socket.io-client/build/typed-events').DefaultEventsMap, import('socket.io-client/build/typed-events').DefaultEventsMap, import('socket.io-client/build/typed-events').DefaultEventsMap>}
  */
 let ioScreen = null
 
 /**
  * Function that starts the application, serving the main "index.html" and
- * making public the "dist" folder
+ * making public the "dist" folder.
  */
 function setupServer() {
   const app = express()
@@ -53,7 +64,6 @@ function setupServer() {
 
   router.get('/', (_, res) => {
     const sKeys = Object.keys(screens).map((s) => parseInt(s))
-    console.log(sKeys)
 
     if (sKeys.length === screenAmount) {
       res.send('Screen limit reached!')
@@ -65,7 +75,7 @@ function setupServer() {
     }
   })
 
-  router.get('/screen/:screenNumber', (req, res) => {
+  router.get('/screen/:screenNumber', (_, res) => {
     res.sendFile(path.resolve(__dirname, '../dist/index.html'))
   })
 
@@ -74,18 +84,30 @@ function setupServer() {
   httpServer.listen(process.env.PORT || 8080)
 
   console.log('Listening on port ' + process.env.PORT || 8080)
+  console.log('Expected display amount: ' + screenAmount)
 }
 setupServer()
 
+/**
+ * Method that setups the screen socket server for multiple screen connection.
+ */
 function setupSocketScreen() {
   ioScreen.on('connection', (socket) => {
     console.log('Connected: ' + socket.id)
 
-    socket.on('connectScreen', (location, callback) => {
-      const screenN = location.split('/screen')[1].split('/')[0]
-      const screen = setScreen(socket.id, screenN, 600, 400)
+    /**
+     * Sets the new screen into screens object.
+     *
+     * @param {number} number - The screen number.
+     * @param {number} width - The screen width.
+     * @param {number} height - The screen height.
+     * @param {(screen: Screen) => void} callback - A function to be called once the screen has been set.
+     */
+    function connectScreen(number, width, height, callback) {
+      const screen = setScreen(socket.id, number, width, height)
       callback(screen)
-    })
+    }
+    socket.on('connectScreen', connectScreen)
 
     socket.emit('test-index', 'Hello from server')
   })
@@ -94,7 +116,7 @@ setupSocketScreen()
 
 /**
  * Method that setups the socket client for connecting the game with rooms
- * and allowing the multiplayer
+ * and allowing the multiplayer.
  */
 function setupSocketClient() {
   const socket = io(process.env.SOCKET_URL || 'http://localhost:3001')
@@ -127,6 +149,8 @@ setupSocketClient()
  * @returns The connected screen data.
  */
 function setScreen(id, screenN, width, height) {
+  screenN = screenN.toString()
+
   if (screens[screenN]) {
     screens[screenN].id = id
     return screens[screenN]
@@ -144,79 +168,3 @@ function setScreen(id, screenN, width, height) {
   screens[newScreenN.toString()] = screen
   return screen
 }
-
-// async function connectScreen(screen) {
-//   screenSocket = io(process.env.MASTER_SCREEN_URL || 'http://localhost:8000')
-
-//   console.log(screenSocket.id)
-
-//   /**
-//    * Function that informs the user if the connection was successed
-//    */
-//   function connect() {
-//     console.log('Connected: ' + screenSocket.id)
-//   }
-//   screenSocket.on('connect', connect)
-
-//   /**
-//    * Function that informs the user if the connection was failed
-//    */
-//   function disconnect() {
-//     console.log('Connected to server: ' + screenSocket.id)
-//   }
-//   screenSocket.on('disconnect', disconnect)
-
-//   const width = 600
-//   const height = 300
-
-//   return new Promise((resolve) => {
-//     screenSocket.emit('connectScreen', screen, width, height, (response) => {
-//       screenNumber = response.number
-//       screenId = response.id
-//       resolve(response)
-//     })
-//   })
-// }
-
-/**
- * Function that starts the application
- */
-// function setupServer() {
-//   const app = express()
-//   httpServer = createServer(app)
-//   setupSocketServer()
-
-//   app.get('/', async (_, res) => {
-//     if (screens.length === screenAmount) {
-//       res.send('Screen limit reached!')
-//     } else {
-//       await connectScreen(null)
-//       res.redirect('/screen/' + screenNumber)
-//       res.end()
-//     }
-//   })
-
-//   app.get('/screen/:screen', (req, res) => {
-//     const _screenNumber = req.params.screen
-
-//     /**
-//      * @type {Screen}
-//      */
-//     const screen = screens[_screenNumber]
-
-//     if (screen && screen.isConnected) {
-//       res.send(screenNumber.toString())
-//     } else if (screen) {
-//       connectScreen(_screenNumber)
-//       res.send(screenNumber.toString())
-//     } else {
-//       connectScreen(_screenNumber)
-//       res.send(_screenNumber.toString())
-//     }
-//   })
-
-//   httpServer.listen(+(process.env.SCREEN_PORT || 8000), () => {
-//     console.log('Screen listening on port ' + process.env.SCREEN_PORT || 8000)
-//   })
-// }
-// setupServer()
