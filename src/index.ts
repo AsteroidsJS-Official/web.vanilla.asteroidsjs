a4719efc35aea78352fbc0bc103628bcb0b5ad2a
Michell Algarra <michell.algarra@gmail.com>
2021-08-24 00:20:59 -0300

    feat(index.ts): removed socket configuration and improved the file organization

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import io from 'socket.io-client/dist/socket.io.js'
-
 import './global.scss'
+import { socket } from './assets/ts/socket'
+
 import { GameFactory } from './assets/ts/engine/game.factory'
 import { Manager } from './assets/ts/entities/manager.entity'
 import { IScreen } from './assets/ts/interfaces/screen.interface'
 
-const socket = io(`http://localhost:8080`)
+const connectScreenData = {
+  number: window.location.pathname.split('/screen/')[1].split('/')[0],
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 
-socket.emit(
-  'connect-screen',
-  window.location.pathname.split('/screen/')[1].split('/')[0],
-  window.innerWidth,
-  window.innerHeight,
-  (response: IScreen) => {
-    console.log(response)
-    bootstrap(response)
-  },
-)
+function connectScreen(response: IScreen): void {
+  console.log(response)
+  bootstrap(response)
+}
+socket.emit('connect-screen', connectScreenData, connectScreen)
 
 function bootstrap(response: IScreen): void {
   const game = GameFactory.create({
     bootstrap: [Manager],
+    screenNumber: response.number,
     width: response.width,
     height: response.height,
   })
