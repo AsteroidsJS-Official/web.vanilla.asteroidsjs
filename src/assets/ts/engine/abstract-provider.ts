9eb4e13764edd926ebf2e14f00306dae0bd237e7
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-08-27 22:34:49 -0300

    feat(abstract-provider.ts): created the "getProvider" method

--- src/assets/ts/engine/abstract-provider.ts
+++ src/assets/ts/engine/abstract-provider.ts
@@ -1,6 +1,22 @@
+import { Type } from './interfaces/type.interface'
+
 /**
  * Class that represents some service in the game
  */
 export abstract class AbstractProvider {
   public constructor(public providers: AbstractProvider[] = []) {}
+
+  /**
+   * Method that returns some sibling component, attached to the same parent
+   * entity
+   *
+   * @param component defines the component type
+   * @returns an object that represents the component instance, attached to
+   * the same parent entity
+   */
+  public getProvider<T extends AbstractProvider>(component: Type<T>): T {
+    return this.providers.find(
+      (c) => c.constructor.name === component.name,
+    ) as T
+  }
 }
