0f1dac2a7a29fb6adbb4947abfcdfae36b9c3c5f
Michell Algarra <michell.algarra@gmail.com>
2021-09-01 02:24:38 -0300

    feat(entities/asteroid): renamed file and implemented the asteroid entity

---
+++ src/assets/ts/entities/asteroid.entity.ts
@@ -0,0 +1,103 @@
+import { socket } from '../socket'
+
+import { isOverflowingX, isOverflowingY } from '../engine/utils/overflow'
+
+import { RenderOverflow } from '../components/render-overflow.component'
+import { Render } from '../components/render.component'
+import { Rigidbody } from '../components/rigidbody.component'
+import { Transform } from '../components/transform.component'
+import { AbstractEntity } from '../engine/abstract-entity'
+import { Entity } from '../engine/decorators/entity.decorator'
+import { IDraw } from '../engine/interfaces/draw.interface'
+import { IOnAwake } from '../engine/interfaces/on-awake.interface'
+import { IOnLoop } from '../engine/interfaces/on-loop.interface'
+import { IOnStart } from '../engine/interfaces/on-start.interface'
+import { Rect } from '../engine/math/rect'
+
+@Entity({
+  components: [Transform, Rigidbody, Render],
+})
+export class Asteroid
+  extends AbstractEntity
+  implements IOnAwake, IOnStart, IDraw, IOnLoop
+{
+  private transform: Transform
+
+  private rigidbody: Rigidbody
+
+  private _asteroidSize: number
+
+  public set asteroidSize(size: number) {
+    this._asteroidSize = size
+  }
+
+  public onAwake(): void {
+    this.transform = this.getComponent(Transform)
+    this.rigidbody = this.getComponent(Rigidbody)
+  }
+
+  public onStart(): void {
+    this.transform.dimensions = new Rect(
+      15 * (this._asteroidSize + 2),
+      15 * (this._asteroidSize + 2),
+    )
+  }
+
+  public onLoop(): void {
+    const overflowingX = isOverflowingX(
+      this.game.getContext().canvas.width,
+      this.transform.position.x,
+      this.transform.totalDimensions.width,
+    )
+    const overflowingY = isOverflowingY(
+      this.game.getContext().canvas.height,
+      this.transform.position.y,
+      this.transform.totalDimensions.height,
+    )
+
+    if (
+      !overflowingX &&
+      !overflowingY &&
+      this.getComponent(Render) &&
+      !this.getComponent(RenderOverflow)
+    ) {
+      this.addComponent(RenderOverflow)
+      this.destroy(this.getComponent(Render))
+    }
+
+    socket.emit('update-slaves', {
+      id: this.id,
+      data: {
+        position: this.transform.position,
+        rotation: this.transform.rotation,
+      },
+    })
+  }
+
+  public draw(): void {
+    this.drawCircle()
+  }
+
+  private drawCircle(): void {
+    this.game
+      .getContext()
+      .translate(
+        this.transform.canvasPosition.x,
+        this.transform.canvasPosition.y,
+      )
+
+    this.game.getContext().beginPath()
+    this.game.getContext().fillStyle = '#484848'
+    this.game
+      .getContext()
+      .arc(0, 0, this.transform.dimensions.width / 2, 0, 2 * Math.PI)
+    this.game.getContext().fill()
+
+    this.game
+      .getContext()
+      .translate(
+        -this.transform.canvasPosition.x,
+        -this.transform.canvasPosition.y,
+      )
+  }
+}
