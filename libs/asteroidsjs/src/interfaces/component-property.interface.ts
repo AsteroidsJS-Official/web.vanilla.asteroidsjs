279025c0d6103cce53910b897bd19711b70f7015
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-04 18:05:20 -0300

    feat(component-property.interface.ts): removed this unused file

--- libs/asteroidsjs/src/interfaces/component-property.interface.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractComponent } from '../abstract-component'
-
-import { Type } from './type.interface'
-
-export interface IComponentProperty {
-  for: Type<AbstractComponent>
-  use: Record<string, unknown>
-}
