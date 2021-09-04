3422a004f41606c1098509a14d0aa498d9abe44b
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-03 23:52:04 -0300

    feat(math.ts): created the "angleToVector2" and the "vector2ToAngle" functions

--- libs/asteroidsjs/src/math/utils.ts
+++ libs/asteroidsjs/src/math/utils.ts
@@ -1,3 +1,5 @@
+import { Vector2 } from '.'
+
 /**
  * Function that, passed a number, returns it module or absolute value
  *
@@ -6,4 +8,27 @@
  */
 export function abs(value: number): number {
   return value < 0 ? -value : value
+}
+
+/**
+ * Function that, passed an angle, returns a vector based on that angle
+ *
+ * @param angle defines the vector angle
+ * @returns the calculated vector
+ */
+export function angleToVector2(angle: number): Vector2 {
+  return new Vector2(Math.cos(angle), Math.sin(angle))
+}
+
+/**
+ * Function that, passed the a vector, returns it angle using the whole
+ * trigonometry circle
+ *
+ * @param vector defines an objecto that represents the vector
+ * @returns a number that represent the vector angle
+ */
+export function vector2ToAngle(vector: Vector2): number {
+  const normalized = vector.normalized
+  const value = Math.atan2(normalized.y, normalized.x)
+  return value < 0 ? 2 * Math.PI + value : value
 }
