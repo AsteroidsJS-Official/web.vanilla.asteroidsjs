5e6cd826c162d36b96b7737e17e526737579fa64
DESKTOP-R7UNTKN\caiqu <caiquetorres.bastos@hotmail.com>
2021-09-07 16:26:17 -0300

    feat(abstract-scene.ts): created this class that represents a scene object in the game, it means that all the game entities will be handled

---
+++ libs/asteroidsjs/src/abstract-scene.ts
@@ -0,0 +1,70 @@
+import { AbstractEntity, IContext, Type } from '..'
+
+import { IAsteroidsApplication } from './interfaces/asteroids-application.interface'
+import { IInstantiateOptions } from './interfaces/instantiate-options.interface'
+
+/**
+ * The scene represents a handler for entities in the game.
+ *
+ * Scenes can be created using the `load` method and passing the
+ * scene class type as parameter. When a scene is unloaded, through the
+ * `unload` method, all it child entities are destroyed like it
+ * components.
+ */
+export abstract class AbstractScene {
+  constructor(
+    readonly id: number | string,
+    readonly game: IAsteroidsApplication,
+    public entities: AbstractEntity[],
+  ) {}
+
+  /**
+   * Method that can create a new project scene
+   *
+   * @param scene defines the scene type
+   * @returns the created scene
+   */
+  load<S extends AbstractScene>(scene: Type<S>): S {
+    return this.game.load(scene)
+  }
+
+  /**
+   * Method that unloads some specified scene.
+   *
+   * @param scene defines the scene id, type or instance
+   */
+  unload<S extends AbstractScene>(scene: string | S | Type<S>): void {
+    this.game.unload(scene)
+  }
+
+  /**
+   * Method that can create new entities
+   *
+   * @param options defines the entity options when intantiating it
+   * @returns the created entity
+   */
+  instantiate<E extends AbstractEntity>(
+    options?: IInstantiateOptions<E>,
+  ): E extends AbstractEntity ? E : AbstractEntity {
+    options.scene ??= this
+    return this.game.instantiate(options)
+  }
+
+  /**
+   * Method that returns the game context
+   *
+   * @returns an object that represents the game context
+   */
+  getContext(): IContext {
+    return this.game.getContext()
+  }
+
+  /**
+   * Method that returns all the entities instantiated in this scene
+   *
+   * @returns an array with all the found entities
+   */
+  getEntities(): AbstractEntity[] {
+    return this.entities
+  }
+}
