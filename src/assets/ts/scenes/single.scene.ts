f116b5ccf91a4ba015b1209b7fcc1151952deab1
DESKTOP-R7UNTKN\caiqu <caiquetorres.bastos@hotmail.com>
2021-09-08 23:24:11 -0300

    feat(single.scene.ts): created this scene to represente the single player game mode

---
+++ src/assets/ts/scenes/single.scene.ts
@@ -0,0 +1,13 @@
+import { Scene, AbstractScene, IOnStart } from '@asteroidsjs'
+
+import { Background } from '../entities/background.entity'
+import { Manager } from '../entities/manager.entity'
+
+@Scene()
+export class Single extends AbstractScene implements IOnStart {
+  onStart(): void {
+    this.createCanvas()
+    this.instantiate({ entity: Background })
+    this.instantiate({ entity: Manager })
+  }
+}
