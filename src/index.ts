b28be42fdd540d7540cada7c3e78cde60df7fdb2
Michell Algarra <michell.algarra@gmail.com>
2021-09-15 21:28:00 -0300

    feat(index.ts): changed the menu scene by the lg screen scene

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import './assets/ts/menu.ts'
 
 import { AsteroidsFactory } from '@asteroidsjs'
 
-import { Menu } from './assets/ts/scenes/menu.scene'
+import { LGScreen } from './assets/ts/scenes/lg-screen.scene'
 
 /**
  * Creates and starts the game.
  */
 function bootstrap(): void {
   const game = AsteroidsFactory.create({
-    bootstrap: [Menu],
+    bootstrap: [LGScreen],
   })
   game.start()
 }
