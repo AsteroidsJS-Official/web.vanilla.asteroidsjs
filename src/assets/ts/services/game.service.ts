37f52f87953dacc83074016cdbf992f1d41e5bc3
Michell Algarra <michell.algarra@gmail.com>
2021-09-19 22:43:33 -0300

    feat(game.service): created the 'asteroidsAmount' property

--- src/assets/ts/services/game.service.ts
+++ src/assets/ts/services/game.service.ts
@@ -7,7 +7,16 @@ import { BehaviorSubject, Observable } from 'rxjs'
  */
 @Service()
 export class GameService extends AbstractService {
+  /**
+   * Property responsible for keeping whether the player died.
+   */
   private _gameOver = new BehaviorSubject<boolean>(false)
+
+  /**
+   * Property responsible for keeping the current asteroids amount
+   * on the game.
+   */
+  private _asteroidsAmount = new BehaviorSubject<number>(0)
 
   public get gameOver$(): Observable<boolean> {
     return this._gameOver.asObservable()
@@ -19,5 +28,17 @@ export class GameService extends AbstractService {
 
   public set gameOver(value: boolean) {
     this._gameOver.next(value)
+  }
+
+  public get asteroidsAmount$(): Observable<number> {
+    return this._asteroidsAmount.asObservable()
+  }
+
+  public get asteroidsAmount(): number {
+    return this._asteroidsAmount.value
+  }
+
+  public set asteroidsAmount(value: number) {
+    this._asteroidsAmount.next(value)
   }
 }
