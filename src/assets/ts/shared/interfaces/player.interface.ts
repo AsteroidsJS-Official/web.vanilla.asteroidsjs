28ee968b29a6d7f1381264ca594ae516554047d1
Michell Algarra <michell.algarra@gmail.com>
2021-09-21 22:43:23 -0300

    feat(player.interface): created the player interface

---
+++ src/assets/ts/shared/interfaces/player.interface.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface that represents the player data.
+ */
+export interface IPlayer {
+  /**
+   * Property that represents the player nickname.
+   */
+  nickname: string
+
+  /**
+   * Property that represents the player color.
+   */
+  color: string
+}
