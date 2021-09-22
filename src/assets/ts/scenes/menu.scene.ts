a64c0de65a580d3bca918bc25d66df4a4127d6af
Michell Algarra <michell.algarra@gmail.com>
2021-09-21 22:52:33 -0300

    feat(menu.scene): instantiated the auth entity to menu scene

--- src/assets/ts/scenes/menu.scene.ts
+++ src/assets/ts/scenes/menu.scene.ts
@@ -3,6 +3,7 @@ import { Scene, AbstractScene, IOnStart } from '@asteroidsjs'
 import { ManagerAsteroids } from '../objects/asteroid/entities/manager-asteroids.entity'
 import { Background } from '../ui/background/entities/background.entity'
 import { Menu as MenuEntity } from '../ui/menu/entities/menu.entity'
+import { Auth } from '../ui/modal/entities/auth.entity'
 
 @Scene()
 export class Menu extends AbstractScene implements IOnStart {
@@ -12,6 +13,7 @@ export class Menu extends AbstractScene implements IOnStart {
     })
     this.instantiate({ entity: Background })
     this.instantiate({ entity: MenuEntity })
+    this.instantiate({ entity: Auth })
     this.instantiate({ entity: ManagerAsteroids, use: { isMenu: true } })
   }
 }
