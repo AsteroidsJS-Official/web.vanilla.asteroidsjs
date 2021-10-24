587201ca2b4ca7dd71b498c1fdcc8a53f97f1247
Erick Escagion <erickeescagion@gmail.com>
2021-10-24 13:21:08 -0300

    test(rigidbody.component.test.ts): created a test to validate that the rigidbody is moving when a velocity is added to it

--- src/assets/ts/tests/rigidbody.component.test.ts
+++ src/assets/ts/tests/rigidbody.component.test.ts
@@ -5,10 +5,12 @@ import {
   AsteroidsFactory,
   Entity,
   AbstractEntity,
+  Vector2,
 } from '@asteroidsjs'
 
 import { Transform } from '../shared/components/transform.component'
 import { Rigidbody } from '../shared/components/rigidbody/rigidbody.component'
+import { delay } from '../utils/delay'
 
 describe('rigidbody', () => {
   @Scene()
@@ -41,4 +43,22 @@ describe('rigidbody', () => {
     const component = entity.getComponent(Rigidbody)
     expect(component).toBeDefined()
   })
+
+  it('should be moving', async () => {
+    const entity = scene.instantiate({
+      entity: TestEntity,
+      components: [Transform, Rigidbody],
+    })
+
+    const rigidbody = entity.getComponent(Rigidbody)
+    const transform = entity.getComponent(Transform)
+
+    const initialPosition = transform.position.clone()
+    rigidbody.velocity = new Vector2(1, 0)
+
+    await delay(1000)
+
+    expect(transform.position.x).not.toBe(initialPosition.x)
+  })
+  
 })
